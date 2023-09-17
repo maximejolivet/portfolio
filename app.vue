a34fc13c1ba1e9c48dc3f8b6afed97f66f82c3f8
Maxime JOLIVET <maximejolivet.pro@gmail.com>
2023-09-17 23:16:08 +0200

    Install Package

--- app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Maxime JOLIVET</h1>
-  </div>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,86 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  ssr: false,
+
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Nuxt 3 Starter Kit",
+      meta: [
+        { name: "description", content: "My amazing Nuxt 3 application." },
+      ],
+      htmlAttrs: {
+        lang: "fr",
+        style: "font-size: 1rem",
+      },
+    },
+
+    pageTransition: {
+      name: "page",
+      mode: "out-in",
+    },
+  },
+
+  modules: [
+    "@nuxt/devtools",
+    // https://i18n.nuxtjs.org/setup
+    // https://github.com/jahidanowar/nuxt3-i18n-tutorial/blob/main/app.vue
+    "@nuxtjs/i18n",
+    // https://tailwindcss.nuxtjs.org/getting-started/setup
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+  ],
+
+  css: ["@/assets/styles/main.scss"],
+
+  nitro: {
+    compressPublicAssets: true,
+  },
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          //
+        },
+      },
+    },
+  },
+
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+
+  i18n: {
+    /* module options */
+    lazy: true,
+    langDir: "locales",
+    strategy: "prefix_except_default",
+    locales: [
+      {
+        code: "fr",
+        iso: "fr-FR",
+        name: "French(FR)",
+        file: "fr-FR.json",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        name: "English(US)",
+        file: "en-US.json",
+      },
+    ],
+    defaultLocale: "fr-FR",
+    vueI18n: "locales/vue-i18n.config.ts",
+  },
+
+  devtools: {
+    // Enable devtools (default: true)
+    enabled: true,
+  },
+});
