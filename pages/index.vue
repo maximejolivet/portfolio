f67b76f695cfd2dc03fcf793cdb07f2ccdb31c31
Maxime JOLIVET <maximejolivet.pro@gmail.com>
2024-03-02 10:05:05 +0100

    Reset Maxime Jolivet

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,9 @@
   <div>
     <div class="max-w-3xl px-8 mt-24 mx-auto">
       <div>
-        <h1 class="text-4xl font-bold text-center">
-          {{ $t("home_title") }}
+        <h1 class="text-xl font-bold text-center">
+          {{ $t("maxime_jolivet") }}
         </h1>
-        <p class="text-center mt-4 text-gray-600">
-          {{ $t("home_btn") }}
-        </p>
       </div>
     </div>
   </div>
