9d7dd64f2be063cea64b4868d1dd31228b0c7671
Maxime JOLIVET <maximejolivet.pro@gmail.com>
2024-03-03 23:13:05 +0100

    Logo Maxime Jolivet White

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <template>
-  <div>
-    <div class="max-w-3xl px-8 mt-24 mx-auto">
-      <div>
-        <h1 class="sr-only font-bold text-center">
-          {{ $t("maxime_jolivet") }}
-        </h1>
-        <div class="logo">
-          <img src="/maxime.svg" alt="Maxime Jolivet" width="200" height="200" />
-        </div>
-      </div>
+  <div class="bg-black">
+    <h1 class="sr-only">
+      {{ $t("maxime_jolivet") }}
+    </h1>
+    <div class="logo">
+      <img src="/maxime.svg" alt="Maxime Jolivet" width="200" height="200" />
     </div>
   </div>
 </template>
