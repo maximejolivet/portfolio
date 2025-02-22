89dc0c799ca499516a78fe1ae4c29cc783942d9b
Maxime JOLIVET <mjolivet@gingerminds.fr>
2025-02-22 22:20:26 +0100

    build(theme): 📦 migrate to tailwind v4 and flat eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,84 +1,99 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
-  ssr: false,
-
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-      title: "Maxime Jolivet - Développeur web",
-      meta: [{ name: "description", content: "Développeur web, Webdesigner" }],
-      htmlAttrs: {
-        lang: "fr",
-        style: "font-size: 1rem",
-      },
-    },
-
-    pageTransition: {
-      name: "page",
-      mode: "out-in",
-    },
-  },
-
   modules: [
-    "@nuxt/devtools",
-    // https://i18n.nuxtjs.org/setup
-    // https://github.com/jahidanowar/nuxt3-i18n-tutorial/blob/main/app.vue
-    "@nuxtjs/i18n",
-    // https://tailwindcss.nuxtjs.org/getting-started/setup
-    "@nuxtjs/tailwindcss",
-    "@pinia/nuxt",
-  ],
-
-  css: ["@/assets/styles/main.scss"],
-
-  nitro: {
-    compressPublicAssets: true,
-  },
-
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          //
-        },
-      },
-    },
-  },
-
-  components: [
-    {
-      path: "~/components",
-      pathPrefix: false,
-    },
-  ],
-
-  i18n: {
-    /* module options */
-    lazy: true,
-    langDir: "locales",
-    strategy: "prefix_except_default",
-    locales: [
+    [
+      '@pinia/nuxt',
       {
-        code: "fr",
-        iso: "fr-FR",
-        name: "French(FR)",
-        file: "fr-FR.json",
-      },
-      {
-        code: "en",
-        iso: "en-US",
-        name: "English(US)",
-        file: "en-US.json",
+        autoImports: ['defineStore', 'storeToRefs'],
       },
     ],
-    defaultLocale: "fr-FR",
-    vueI18n: "locales/vue-i18n.config.ts",
+    'nuxt-security',
+    '@nuxt/eslint',
+  ],
+  imports: {
+    //dirs: ['composables/**'],
   },
-
   devtools: {
-    // Enable devtools (default: true)
-    enabled: true,
+    enabled: false,
   },
-});
+  app: {
+    head: {
+      title: 'Boilerplate',
+      link: [{ rel: 'icon', type: 'image/ico', href: '/favicon.png' }],
+      meta: [
+        {
+          name: 'viewport',
+          content:
+            'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
+        },
+      ],
+    },
+  },
+  css: ['~/assets/css/main.css', '~/assets/css/transition.css'],
+  devServer: {
+    port: 8000,
+  },
+  compatibilityDate: '2025-01-09',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        strict: false,
+        strictNullChecks: true,
+      },
+    },
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: false,
+        quotes: 'single',
+        quoteProps: 'consistent-as-needed',
+        commaDangle: 'always-multiline',
+        blockSpacing: true,
+        arrowParens: true,
+      },
+    },
+  },
+  security: {
+    nonce: true,
+    rateLimiter: false,
+    csrf: true,
+    headers: {
+      contentSecurityPolicy: {
+        'script-src': [
+          // self             => Fallback value, will be ignored by most modern browsers (level 3)
+          // unsafe-inline    => Fallback value, will be ignored by almost any browser (level 2)
+          // strict-dynamic   => Strict CSP via 'strict-dynamic', supported by most modern browsers (level 3)
+          // nonce-{{nonce}}  => Enables CSP nonce support for scripts in SSR mode, supported by almost any browser (level 2)
+          '\'self\'',
+          '\'unsafe-inline\'',
+          '\'strict-dynamic\'',
+          '\'nonce-{{nonce}}\'',
+        ],
+        'style-src': [
+          // self           => Enables loading of stylesheets hosted on same origin
+          // unsafe-inline  => Recommended default for most Nuxt apps
+          '\'self\'',
+          'fonts.googleapis.com',
+          '\'unsafe-inline\'',
+        ],
+        // img-src => Add relevant https://... sources if you load images from external sources
+        'base-uri': ['\'none\''],
+        'img-src': ['\'self\'', 'data:'],
+        'font-src': ['\'self\'', 'fonts.gstatic.com'],
+        'object-src': ['\'none\''],
+        'script-src-attr': ['\'none\''],
+        'frame-ancestors': ['\'self\''],
+        'upgrade-insecure-requests': true,
+      },
+      permissionsPolicy: {
+        camera: ['self'],
+      },
+    },
+  },
+})
