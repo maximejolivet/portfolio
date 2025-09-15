33b3aedfd43e0690947994cdb8645efd1b61e53b
Maxime JOLIVET <mjolivet@gingerminds.fr>
2025-09-15 22:02:16 +0200

    Install i18n, update package dependencies

---
+++ pages/error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center items-center text-center p-4">
+    <h1 class="text-4xl font-bold mb-4">{{ error.statusCode }}</h1>
+    <p class="text-lg mb-6">{{ message }}</p>
+    <NuxtLink to="/" class="text-blue-500 underline">
+      {{ $t('error.back_home') }}
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  error: {
+    type: Object,
+    required: true,
+  },
+})
+
+const message = computed(() => {
+  if (error.statusCode === 404) {
+    return useI18n().t('error.page_not_found')
+  }
+  return useI18n().t('error.generic')
+})
+</script>
