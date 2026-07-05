287134ddd1c571b9db35fe42db8b859ccc885e47
Maxime <maximejolivet.pro@gmail.com>
2026-07-05 22:56:01 +0200

    Rewrite Projects page components in editorial style

--- components/sections/ProjectsSection.vue
+++ components/sections/ProjectsSection.vue
@@ -9,16 +9,12 @@ const { t } = useI18n()
     <UiSectionHeading :eyebrow="$t('projectsSection.eyebrow')" align="center" class="mx-auto">
       <template #title>
         {{ $t('projectsSection.titleStart') }}
-        <UiGradientText>{{ $t('projectsSection.titleHighlight') }}</UiGradientText>
+        <span class="text-accent">{{ $t('projectsSection.titleHighlight') }}</span>
         {{ $t('projectsSection.titleEnd') }}
       </template>
     </UiSectionHeading>
 
-    <AnimationsScrollReveal
-      as="div"
-      :stagger="0.08"
-      class="mt-14 grid gap-6 sm:grid-cols-2 lg:grid-cols-3"
-    >
+    <div class="mt-14 grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
       <CardsClientSectorCard
         v-for="sector in CLIENT_SECTORS"
         :key="sector.id"
@@ -26,6 +22,6 @@ const { t } = useI18n()
         :icon="sector.icon"
         :clients="sector.clients"
       />
-    </AnimationsScrollReveal>
+    </div>
   </LayoutPageSection>
 </template>
